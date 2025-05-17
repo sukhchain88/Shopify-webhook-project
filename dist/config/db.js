15b18e6f6c6bcc3151a2a1ee9d806310d827e1ae
sukhchain <parmjeet2149@gmail.com>
2025-05-17 12:35:46 +0530

    fixing db file

--- dist/config/db.js
+++ dist/config/db.js
@@ -2,11 +2,17 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { Sequelize } from "sequelize";
-import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME, } from "../config/config.js";
+import {
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+} from "../config/config.js";
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    host: DB_HOST,
-    port: parseInt(DB_PORT || "5432", 10),
-    dialect: "postgres",
-    logging: process.env.NODE_ENV === "development" ? console.log : false,
+  host: DB_HOST,
+  port: parseInt(DB_PORT || "5432"),
+  dialect: "postgres",
+  logging: process.env.NODE_ENV === "development" ? console.log : false,
 });
 export default sequelize;
--- src/utils/validateWebhookSignature.ts
+++ src/utils/validateWebhookSignature.ts
@@ -21,3 +21,4 @@ export const validateWebhookSignature = (
     return false;
   }
 };
+ 
